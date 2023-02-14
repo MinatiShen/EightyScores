e3fca017d4d00d2e9b652ea438e86db3c27b4492
Tongchuan Shen <ts6@illinois.edu>
2023-02-14 07:43:41 +0000

    Initial submission for card class

---
+++ src/modules/card.js
@@ -0,0 +1,41 @@
+class Card {
+    constructor(suit, rank) {
+      this.suit = suit;
+      this.rank = rank;
+    }
+  
+    getSuit() {
+      return this.suit;
+    }
+  
+    getRank() {
+      return this.rank;
+    }
+  
+    getValue() {
+      switch (this.rank) {
+        case 'B':
+          return 16;
+        case 'S':
+          return 15;
+        case 'A':
+          return 14;
+        case 'K':
+          return 13;
+        case 'Q':
+          return 12;
+        case 'J':
+          return 11;
+        default:
+          return parseInt(this.rank);
+      }
+    }
+  
+    toString() {
+      return this.rank + this.suit;
+    }
+    
+    isSmaller(other){
+        return this.getValue()<other.getValue();
+    }
+  }
---
+++ src/modules/card.test.js
@@ -0,0 +1,18 @@
+const Card = require('./card');
+
+describe('Card', () => {
+  test('returns correct value for ace of spades', () => {
+    let card = new Card('S', 'A');
+    expect(card.getValue()).toBe(11);
+  });
+
+  test('returns correct value for queen of hearts', () => {
+    let card = new Card('H', 'Q');
+    expect(card.getValue()).toBe(10);
+  });
+
+  test('returns correct string representation', () => {
+    let card = new Card('D', '9');
+    expect(card.toString()).toBe('9D');
+  });
+});
